4729a8c65901e554372481de22b596e03315123f
spelkonen <g7753@student.jamk.fi>
2014-07-25 10:26:31 +0300

    Initial commit

---
+++ InteractionScripts/WebInteractionHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Awesomium.Core;
+using Awesomium.Unity;
+
+public class WebInteractionHandler : WebUIScript {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		JSObject myJSObject = webUI.CreateGlobalJavascriptObject ("myJSObject");
+		if (myJSObject == null) {
+			return;
+		} 
+
+		using (myJSObject) {
+			myJSObject.Bind("onButtonClick", false, myJSMethodHandler);
+		}
+	}
+
+	private void myJSMethodHandler(object sender, JavascriptMethodEventArgs args) {
+		if (args.MethodName == "onButtonClick") {
+			DebugConsole.Log("Workkii", "normal");
+		}
+	}
+}
